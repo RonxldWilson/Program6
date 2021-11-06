30ca4bf9ff5f0bc84da8418efcf1d8b1e3f1cb31
RonxldWilson <ronaldwilson00000@gmail.com>
2021-11-07 03:33:36 +0530

    Fragment Addition

---
+++ app/src/main/java/com/example/program6/BlankFragment.java
@@ -0,0 +1,4 @@
+package com.example.program6;
+
+public class BlankFragment {
+}
--- app/src/main/java/com/example/program6/MainActivity.java
+++ app/src/main/java/com/example/program6/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.program6;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 /*
 Create a Mobile app that demonstrates the MENU OPTION
 Minimum 4 Option in the menu ( Under 3 dot ) [Send, Color, share, Exit....etc]
@@ -15,10 +19,32 @@ exit - closes the app
 Display the appropriate message/ perform the action for each menu item clicked.
 */
 public class MainActivity extends AppCompatActivity {
+    Button button1, button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1 = (Button)findViewById(R.id.button);
+        button2 = (Button)findViewById(R.id.button2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction f1 = getSupportFragmentManager().beginTransaction();
+                Blankfragment ff1 =  new Blankfragment();
+                f1.replace(R.id.fragmentcontainer, ff1);
+                f1.commit();
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction f2 = getSupportFragmentManager().beginTransaction();
+                Blankfragment ff2 =  new Blankfragment();
+                f2.replace(R.id.fragmentcontainer, ff2);
+                f2.commit();
+            }
+        });
     }
 }
