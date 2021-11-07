9d932c2b5af3420d1e30fd8ef86b510a5bc83b71
RonxldWilson <ronaldwilson00000@gmail.com>
2021-11-07 06:51:52 +0530

    Final Commit

--- app/src/main/java/com/example/program6/MainActivity.java
+++ app/src/main/java/com/example/program6/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.program6;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 /*
 Create a Mobile app that demonstrates the MENU OPTION
@@ -24,12 +35,14 @@ Display the appropriate message/ perform the action for each menu item clicked.
 */
 public class MainActivity extends AppCompatActivity {
     Button button1, button2;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button1 = (Button)findViewById(R.id.button);
         button2 = (Button)findViewById(R.id.button2);
+        editText = (EditText)findViewById(R.id.editTextTextPersonName);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,15 +85,33 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     public void menuSend(){
+//        long int number = "
+        String number = "8359838126";
+        String message = "This message is sent through a custom built app";
+        Toast.makeText(getApplicationContext(), "Opened WhatsApp", Toast.LENGTH_SHORT).show();
+
+        Uri uri = Uri.parse("smsto:" + number);
+        Intent i = new Intent(Intent.ACTION_SENDTO, uri);
+        i.setPackage("com.whatsapp");
+        startActivity(Intent.createChooser(i, ""));
 
     }
     public void menuShare(){
-
+        Toast.makeText(getApplicationContext(), "Sharing the text", Toast.LENGTH_SHORT).show();
+        String message = editText.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        startActivity(intent);
     }
     public void menuColour(){
-
+        LinearLayout Layout;
+        Toast.makeText(getApplicationContext(), "Colour Change", Toast.LENGTH_SHORT).show();
+        Layout = findViewById(R.id.og);
+        Layout.setBackgroundColor(Color.YELLOW);
     }
     public void menuExit(){
-
+        Toast.makeText(getApplicationContext(), "App Exited", Toast.LENGTH_SHORT).show();
+        this.finishAffinity();
     }
 }
